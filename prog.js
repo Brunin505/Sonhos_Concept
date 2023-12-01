50d6a247341486932898d40dc2f8aabc0b702b4a
Bruno Teodoro <bruno_t_santos@estudante.sesisenai.org.br>
2023-12-01 11:16:58 -0300

    soma

--- prog.js
+++ prog.js
@@ -1,7 +1,13 @@
 let res = document.getElementById('res')
 
-function soma(){
 
+function soma(){
+    let num1 = Number(document.getElementById('num1').value)
+    let num2 = Number(document.getElementById('num2').value)
+
+    let total1 = num1 + num2
+    res.innerHTML = "O resultado da soma de " + num1 + " com " + num2 + " se dá pelo resultado de = " + total1.toFixed(2)
+    
 }
 function diminuir(){
     
